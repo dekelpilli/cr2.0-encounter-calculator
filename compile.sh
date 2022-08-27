a8eaa216b328751ff6869a313e51b29c182d8e0f
Dekel Pilli <dekelpilli@gmail.com>
2022-08-27 14:19:23 +1000

    Remove --initialize-at-build-time in favour of graal-build-time

--- compile.sh
+++ compile.sh
@@ -3,8 +3,7 @@ clojure -M -e "(compile 'cr2.core)"
 clojure -M:uberjar --main-class cr2.core
 native-image -jar target/cr2.0-calculator.jar \
         --no-fallback \
-        --initialize-at-build-time \
         --report-unsupported-elements-at-runtime \
-        -H:IncludeResourceBundles=consoleui_messages \
+        -H:IncludeResourceBundles=consoleui_messages,jline.console.completer.CandidateListCompletionHandler \
         -H:ReflectionConfigurationFiles=graal/reflect-config.json \
         -H:+ReportExceptionStackTraces
