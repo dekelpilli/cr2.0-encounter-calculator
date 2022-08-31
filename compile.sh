b9f157a718672b00cd01f6be76a80dcdfac0f893
Dekel Pilli <dekelpilli@gmail.com>
2022-08-31 21:50:32 +1000

    Fix ansi console install on graal on unix shells

--- compile.sh
+++ compile.sh
@@ -1,9 +1,12 @@
 mkdir -p classes
 clojure -M -e "(compile 'cr2.core)"
 clojure -M:uberjar --main-class cr2.core
-native-image -jar target/cr2.0-calculator.jar \
+native-image -jar target/cr2.0-encounter-calculator.jar \
+        -o target/cr2.0-encounter-calculator \
         --no-fallback \
         --report-unsupported-elements-at-runtime \
         -H:IncludeResourceBundles=consoleui_messages,jline.console.completer.CandidateListCompletionHandler \
         -H:ReflectionConfigurationFiles=graal/reflect-config-unix.json \
+        -H:ResourceConfigurationFiles=graal/resources-config.json \
+        -H:JNIConfigurationFiles=graal/jni-config.json \
         -H:+ReportExceptionStackTraces
